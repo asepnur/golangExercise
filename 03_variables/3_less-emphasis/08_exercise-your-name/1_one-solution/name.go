e39b81afda43b278be00cf951b39fb81f6b33382
klikkanan <asepnur.isk@gmail.com>
2017-06-01 17:15:36 +0700

    exercise name variable

---
+++ 03_variables/3_less-emphasis/08_exercise-your-name/1_one-solution/name.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+var name = "Asepnur"
+
+func main() {
+
+	fmt.Println("Hello ", name)
+}
---
+++ 03_variables/3_less-emphasis/08_exercise-your-name/2_another-solution/name.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "Asepnur"
+	fmt.Println("Hello", name)
+}
