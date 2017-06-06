3782fde5b76676943e5993b4adcaa49275d21ece
klikkanan <asepnur.isk@gmail.com>
2017-06-06 11:29:02 +0700

    bland identifier

---
+++ 05_blank-identifier/01_invalid-code/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	name := "Asep Nur Muhammad"
+	//it wil produce error because age not being used
+	age := 29
+	fmt.Println("My name is ", name)
+}
