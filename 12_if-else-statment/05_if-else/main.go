68b8250c797ad8c7c2a62ded3e1e823bbc51aae1
klikkanan <asepnur.isk@gmail.com>
2017-06-10 09:49:35 +0700

    if else statment

---
+++ 12_if-else-statment/05_if-else/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	if false {
+		fmt.Println("this is the first statment")
+	} else {
+		fmt.Println("this is the second statment")
+	}
+}
---
+++ 12_if-else-statment/06_if-else-else/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	if false {
+		fmt.Println("first statment")
+	} else if true {
+		fmt.Println("second statment")
+	} else {
+		fmt.Println("third statment")
+	}
+}
